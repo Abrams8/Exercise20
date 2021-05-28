e2924082e6461ce358b700baab7059348f764b3e
Abrams <dima.abramchik@gmail.com>
2021-05-28 18:20:44 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;                
+
+public class Main {
+
+    public static void main(String[] args) {
+        int array[] = {2, 5, 6, 15, 3, 7, 15, 17, 11, 12};
+        int index = 0;
+        int maxElement = -999999999;
+        int maxElementIndex = 0;
+        for (; index < array.length; index++) {
+            if (maxElement < array[index]) {
+                maxElement = array[index];
+                maxElementIndex = index;
+            }
+        }
+        System.out.println("Индекс максимального элемента массива = " + maxElementIndex);
+    }
+}
