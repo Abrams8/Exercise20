e3763c1f8aa1696aad4d0adb2646cae03d731ed2
Abrams <dima.abramchik@gmail.com>
2021-05-30 03:00:06 +0300

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int array[] = {2, 5, 6, 15, 3, 7, 14, 17, 11, 12};
-        int index = 0;
+        int array[] = {2, 5, 1, 5, 3, 7, 14, 1, 17, 9};
+        int i = 0;
         int maxElement = -999999999;
         int maxElementIndex = 0;
-        for (; index < array.length; index++) {
-            if (maxElement < array[index]) {
-                maxElement = array[index];
-                maxElementIndex = index;
+        for (; i < array.length; i++) {
+            if (maxElement < array[i]) {
+                maxElement = array[i];
+                maxElementIndex = i;
             }
         }
-        System.out.println("Индекс максимального элемента массива = " + maxElementIndex);
+        System.out.print("Индекс(ы) максимального элемента массива = ");
+        for (i = 0; i < array.length; i++) {          //проверка на случай не одного макс значения :)
+            if (array[i] == maxElement) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
