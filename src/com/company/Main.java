09b743063d40fe38edf0001a65723b634c301039
Abrams <dima.abramchik@gmail.com>
2021-05-30 04:02:33 +0300

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        int array[] = {2, 5, 1, 5, 3, 7, 14, 1, 17, 9};
+        int[]array={26,86,79,56,48,25,36,48,75,83};
+        findMaxElementIndex(array);
+
+    }
+    public static void findMaxElementIndex(int [] array) {
         int i = 0;
         int maxElement = -999999999;
         int maxElementIndex = 0;
